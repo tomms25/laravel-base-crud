f37bf6b5183d5a56eafd80c926764e2b38f6cf5f
tomms25 <tommasobaccano@gmail.com>
2023-02-02 16:52:28 +0100

    elaborazione del database tramite factory migrations e seeder

--- database/seeders/SantiSeeder.php
+++ database/seeders/SantiSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Santi;
 
 class SantiSeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class SantiSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Santi::factory()->count(100)->create();
     }
 }
