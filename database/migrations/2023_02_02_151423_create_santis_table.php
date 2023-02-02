940bbdff84874527a883623e219497b3e608c153
tomms25 <tommasobaccano@gmail.com>
2023-02-02 16:22:36 +0100

    first commit

---
+++ database/migrations/2023_02_02_151423_create_santis_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('santis', function (Blueprint $table) {
+            $table->id();
+
+            $table -> string('nome', 32);
+            $table -> string('place_of-birth', 32);
+            $table -> date('given_blessing');
+            $table -> integer('miracles_number')->unsigned();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('santis');
+    }
+};
